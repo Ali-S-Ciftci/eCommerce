ebe4beb65361fc8843a963833057c3a14424e1e9
Ali-S-Ciftci <ali_ciftci@live.com>
2019-11-01 16:47:50 -0400

    added checkout page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import { auth } from '../../firebase/firebase.utils'
 import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+import { selectCartHidden } from '../../redux/cart/cart.selectors'
+import { selectCurrentUser } from '../../redux/user/user.selectors'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
@@ -12,7 +15,6 @@ import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
     <div className='header'>
-        {console.log(currentUser)}
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
         </Link>
@@ -31,9 +33,9 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 )
 
-const mapStateToProps = ({user: { currentUser}, cart: { hidden }}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
